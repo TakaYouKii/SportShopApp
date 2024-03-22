612ba6c094418ac05f2dcdf47b3aa6a200d3a896
TakaYouKii <avwro1708@gmaol.cpm>
2024-03-22 16:15:55 +0600

    создал splash screen

--- app/src/main/java/com/example/sportshopapp/presentation/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/example/sportshopapp/presentation/onboarding/OnboardingActivity.kt
@@ -1,16 +1,17 @@
-package com.example.sportshopapp
+package com.example.sportshopapp.presentation.onboarding
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.sportshopapp.R
 
-class MainActivity : AppCompatActivity() {
+class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_onboarding)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/sportshopapp/presentation/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,44 @@
+package com.example.sportshopapp.presentation.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.findNavController
+import com.example.sportshopapp.R
+import com.example.sportshopapp.presentation.main.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        // Найдем ваш корневой Layout (вместо R.id.main используйте id вашего корневого Layout)
+        val rootLayout = findViewById<View>(android.R.id.content)
+
+        // Установим слушатель для обработки изменений insets
+        ViewCompat.setOnApplyWindowInsetsListener(rootLayout) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            // Установим отступы для вашего корневого Layout с учетом системных баров
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+            // Вернем объект WindowInsetsCompat, обновленный insets
+            insets
+        }
+
+        // Переход к основной активити через 3 секунды
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Закрыть SplashScreenActivity после перехода на MainActivity
+        }, 3000)
+    }
+}
