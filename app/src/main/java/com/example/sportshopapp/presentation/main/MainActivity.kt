5f8b3a9da0b8c8059b59c52ca91d05062e348132
TakaYouKii <avwro1708@gmaol.cpm>
2024-03-24 21:16:17 +0600

    Создал нижнюю навигацию экрана Главной Категории Корзины Профиль

--- app/src/main/java/com/example/sportshopapp/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/sportshopapp/presentation/main/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.sportshopapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btmNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val controller = findNavController(R.id.fragmentControllerView)
+        btmNavView.setupWithNavController(controller)
     }
 }
