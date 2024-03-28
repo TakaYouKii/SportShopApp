2d64f2ac4976c3490ff712ef6db3f000896c3305
TakaYouKii <avwro1708@gmaol.cpm>
2024-03-28 20:47:31 +0600

    Добавил изменения в нижнюю навигацию

--- app/src/main/java/com/example/sportshopapp/presentation/main/FavouriteFragment.kt
+++ app/src/main/java/com/example/sportshopapp/presentation/main/FavouriteFragment.kt
@@ -1,21 +1,23 @@
 package com.example.sportshopapp.presentation.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
 import com.example.sportshopapp.R
 
+class FavouriteFragment : Fragment() {
 
-class CategoryFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        return inflater.inflate(R.layout.fragment_favourite, container, false)
     }
+
 
 }
